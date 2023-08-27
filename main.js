dc3499ae6acb32119ea23968699a85bc19b163d4
alphaleadership <alphaleadership.yt@gmail.com>
2023-08-27 17:05:58 +0200

    maintenant sa marche

--- main.js
+++ main.js
@@ -1,33 +1,30 @@
-// Importer le module 'readline' pour lire l'entrée depuis le terminal
 const readline = require("readline");
-const rules=[
-  ["A","#"],
-  ["l","!"],
-  ["E","€"]
-]
-// Créer une interface de lecture
+
+const rules = [
+  ["A", "#"],
+  ["l", "!"],
+  ["E", "€"]
+];
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-// Fonction pour remplacer les caractères
 function remplacerCaracteres(phrase) {
-
+  let nouvellePhrase = phrase; // Initialise nouvellePhrase avec la phrase d'origine
 
   for (let i = 0; i < rules.length; i++) {
-    phrase.replace(rules[i][0],rules[i][1])
+    console.log(nouvellePhrase);
+    nouvellePhrase = nouvellePhrase.split(rules[i][0]).join(rules[i][1])
   }
 
-  let nouvellePhrase = phrase
-  nouvellePhrase = nouvellePhrase.replace(/\s+/g, ""); // Supprimer les espaces
   return nouvellePhrase;
 }
 
-// Demander à l'utilisateur de saisir une phrase
 rl.question("Entrez une phrase : ", (phrase) => {
   let phraseModifiee = remplacerCaracteres(phrase);
   console.log("Phrase modifiée :", phraseModifiee);
 
-  rl.close(); // Fermer l'interface de lecture
+  rl.close();
 });
